c20ebbee99853acdda9a609d5b34d17466fab236
Goh Yon Cing <55395418+Yoncing99@users.noreply.github.com>
2020-04-11 19:01:52 +0800

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,22 @@
+class QuestionOne {
+	private int count;
+	
+	public QuestionOne(){
+		count = 1;
+	}
+	public void increment() {
+		count = count + 1;
+	}
+	public int getCount() {
+		return count;
+	}
+}
+public class Q1 {
+	public static void main (String []arg) {
+		QuestionOne q1;
+		q1 = new QuestionOne();
+		q1.increment();
+		q1.increment();
+		System.out.println(q1.getCount());
+	}
+}
